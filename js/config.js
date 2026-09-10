273b5e396e4d3ab47ede27d744a2a341d297c742
md-muzaffar <mdmuzaffar2257@gmail.com>
2026-09-10 11:20:50 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,4 +3,4 @@
 // and the README), paste the Web App URL below. It looks like:
 // https://script.google.com/macros/s/AKfycb..../exec
 // ---------------------------------------------------------------
-const REGISTRATION_ENDPOINT = "PASTE_YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE";
+const REGISTRATION_ENDPOINT = "https://script.google.com/macros/s/AKfycbwfPkOuN0BpzrYYdiAM7Ar84juQwlq4U-vNxfGufxW96_6M6iPA-hQlKUq-qte9NoNOEQ/exec";
